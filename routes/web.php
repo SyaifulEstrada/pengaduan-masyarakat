4421c6bf6fd691002b68cd867ecbb2584b9939bb
SyaifulEstrada <syaifultheminister@gmail.com>
2023-02-20 21:34:04 +0700

    'First'

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\User\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', [UserController::class, 'index'])->name('pekat.index');
+
+Route::post('/login/auth', [UserController::class, 'login'])->name('pekat.login');
+
+Route::get('/register', [UserController::class, 'formRegister'])->name('pekat.formRegister');
+Route::post('/register/auth', [UserController::class, 'register'])->name('pekat.register');
+
+Route::post('/store', [UserController::class, 'storePengaduan'])->name('pekat.store');
+Route::get('/laporan/{siapa?}', [UserController::class, 'laporan'])->name('pekat.laporan');
+
+Route::get('/logout', [UserController::class, 'logout'])->name('pekat.logout');
